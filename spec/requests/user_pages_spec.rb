f67bea80a7c617af1eb37f7fffd084ccfd5290f2
Jeff Morris <jeffreylee3@gmail.com>
2013-01-11 13:12:07 -0600

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -62,10 +62,19 @@ describe "UserPages" do
 
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
   	before { visit user_path(user) }
 
   	it { should have_selector('h1', text: user.name) }
   	it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
 
@@ -144,10 +153,7 @@ describe "UserPages" do
       it { should have_selector('div.alert.alert-success') }
       it { should have_link('Sign out', href: signout_path) }
       specify { user.reload.name.should  == new_name }
-      specify { user.reload.email.should == new_email }    
+      specify { user.reload.email.should == new_email } 
     end
   end
-
-
-
 end
